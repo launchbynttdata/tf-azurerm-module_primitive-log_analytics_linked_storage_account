ef1b180a8cda745413c56660b6feb4cb4d9baa77
343499_nttds <ASMSaifool.Islam@nttdata.com>
2026-08-31 12:42:24 -0400

    feat!: convert to copier-managed skeleton

--- tests/post_deploy_functional_readonly/main_test.go
+++ tests/post_deploy_functional_readonly/main_test.go
@@ -33,5 +33,5 @@ func TestLinkedAccountModule(t *testing.T) {
 		SetTestConfigFileName(infraTFVarFileNameDefault).
 		Build()
 
-	lib.RunSetupTestTeardown(t, *ctx, testimpl.TestComposableComplete)
+	lib.RunNonDestructiveTest(t, *ctx, testimpl.TestComposableComplete)
 }
